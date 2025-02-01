f0424b600e51480f45637aa096b83effa22f1bae
Gopi <dhavagopi8816@gmail.com>
2025-02-01 15:05:25 +0530

    first commit

--- Student Management/colors1.py
+++
@@ -1,7 +0,0 @@
-color_1 = "gray95"
-color_2 = "pink"
-color_3 = "black"
-color_4 = "white"
-
-font_1 = "times new roman"
-font_2 = "helvetica"
--- Student Management/database1.py
+++
@@ -1,5 +0,0 @@
-# User database
-host = 'localhost'
-user = 'root'
-password = 'Gopi@4363'
-database = 'student_management'
